2ebe86aa66a4e92f3c01c8c1a5b30a3333620414
MarioBlasi <mblasi509@gmail.com>
2023-03-15 12:53:10 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*Esercizio di oggi, Lista della spesa con ciclo while
+
+ Consegna:
+ Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) 
+   gli elementi della lista individualmente con un ciclo while.
+
+ Consigli:
+ Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+ Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, 
+ all'interno ciclo whileEsercizio di oggi, Lista della spesa con ciclo while*/
+
+let listaSpesa = [
+  "prodotto1",
+  "prodotto2",
+  "prodotto3",
+  "prodotto4",
+  "prodotto5",
+  "prodotto6",
+  "prodotto7",
+  "prodotto8",
+  "prodotto9",
+  "prodotto10",
+  "prodotto11",
+];
+
+console.log(listaSpesa);
+// while (i <= 20) {
+//     console.log("Numero: ", i);
+//     i++
+// }
